ed77670e0ca39517c8037120fee0ce56343e53b3
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2024-05-21 18:43:06 +0200

    completed me/account and me/account/transactions route

--- backend/src/server.js
+++ backend/src/server.js
@@ -43,28 +43,32 @@ app.post("/users", async (req, res) => {
   // Så innan man sparar användaren kolla att usermname itne redan existerar.
 
   try {
+    // SQL-förfrågan - skapa användare i users-tabellen
     const userResult = await query(
       "INSERT INTO users (username, password) VALUES (?, ?)",
       [username, password]
     );
 
+    // id för användare som lagts till i databasen
     const userId = userResult.insertId;
 
+     // SQL-förfrågan - skapa konto i accounts-tabellen, 0 från start
     const accountResult = await query(
       "INSERT INTO accounts (user_id, amount) VALUES (?, ?)",
       [userId, 0]
     );
 
+    // id för konto som lagts till i databasen
     const accountId = accountResult.insertId;
 
-    res
-      .status(201)
-      .json({
+    // Svar från servern om att användare och konto skapats
+    res.status(201).json({
         message: "User and account created",
         userId: userId,
         accountId: accountId,
       });
   } catch (error) {
+    // Fel i databasuppkoppling, serverkod eller annat
     console.error("Error:", error);
     res.status(500).send("Error creating user");
   }
@@ -76,36 +80,44 @@ app.post("/sessions", async (req, res) => {
   const { username, password } = req.body;
 
   try {
+    // SQL-förfrågan - hämta tabellraden där användarnamnet är username (från req.body)
     const userResult = await query("SELECT * FROM users WHERE username = ?", [
       username,
     ]);
 
-    const user = userResult[0] // Obs! userResult returnerar en array, position 0 är första raden.
+    // userResult returnerar en array, position 0 är första raden
+    const user = userResult[0] 
 
+    // Om ingen användare hittas som matchar username
     if(!user) {
         return res.status(401).send("User not found");
-
     }
 
+    // Om användarens lösenord i databasen matchar password (från req.body)
     if (user.password === password) {
 
+      // Användaren är betrodd - generera engångslösenord
       const token = generateOTP();
 
-      // Lägg till user_id och token i session-tabellen
+      // SQL-förfrågan - Lägg till en session i session-tabellen med det generade engångslösenordet och id för användaren
       const sessionResult = await query(
         "INSERT INTO sessions (user_id, token) VALUES (?, ?)",
         [user.id, token]
       );
       
+      // sessionResult returnerar en array, position 0 är första raden
       const sessionId = sessionResult.insertId;
 
+      // Skicka svar från servern med engångslösenordet och id för sessionen
       res.status(200).json({ message: "Login successful", token: token, sessionId: sessionId });
 
     } else {
+      // Om användaren inte är betrodd...
       return res.status(401).send("Invalid password");
     }
 
   } catch (error) {
+    // Fel i databasuppkoppling, serverkod eller annat
     console.error("Error:", error);
     return res.status(401).send("Error during login");
   }
@@ -143,69 +155,158 @@ app.post("/sessions", async (req, res) => {
 });
 
 // Route för att hämta användarens konton och visa saldo
-app.post("/me/account", (req, res) => {
+app.post("/me/account", async (req, res) => {
   // Extrahera token från Authorization-headern. Exempel på header: "Bearer 12313"
   const token = req.headers.authorization.split(" ")[1];
   console.log("Received token:", token);
 
-  // Hitta sessionen i sessions-arrayen som matchar token
-  const session = sessions.find((session) => session.token === token);
-  console.log("Found session:", session);
+  try {
+      // SQL-förfrågan - hitta sessionen i sessions-tabellen som matchar token
+      const sessionResult = await query("SELECT * FROM sessions WHERE token = ?", [
+        token,
+      ]);
 
-  // Om sessionen hittas, extrahera userId från sessionen
-  if (session) {
-    const userId = session.userId;
+      // sessionResult returnerar en array, position 0 är första raden
+      const session = sessionResult[0];
 
-    // Hitta kontot i accounts-arrayen som matchar userId
-    const account = accounts.find((acc) => acc.userId === userId);
-    if (account) {
-      // Om kontot hittas, skicka tillbaka saldo som JSON-svar
-      res.json({ balance: account.balance });
-    } else {
-      // Om kontot inte hittas, skicka tillbaka status 404 och ett felmeddelande
-      res.status(404).json({ error: "Account not found" });
-    }
-  } else {
-    // Om sessionen inte hittas, skicka tillbaka status 401 och ett felmeddelande
-    res.status(401).json({ error: "Invalid session token" });
+      // Om engångslösenordet inte finns i session-tabellen
+      if(!session) {
+        return res.status(401).send("Invalid session token");
+      }
+
+      // Användar-id för sessionen som motsvarade engångslösenordet
+      const userId = session.user_id;
+
+      // SQL-förfrågan - hitta kontot i accounts-tabellen som matchar userId
+      const accountResult = await query(
+        "SELECT * FROM accounts WHERE user_id = ?",
+        [userId]
+      );
+
+      // accountResult returnerar en array, position 0 är första raden
+      const account = accountResult[0];
+
+      // Om inte kontot hittades
+      if(!account) {        
+        return res.status(401).send("Account not found");
+      }
+
+      // Hämta nuvarande saldo från konto
+      const balance = account.amount;
+
+      // Skicka svar till servern med nuvarnde saldo
+      res.status(200).json({ balance });
+
+  } catch (error) {
+    // Fel i databasuppkoppling, serverkod eller annat
+    console.error("Error fetching balance:", error);
+    res.status(500).send("Error fetching balance");
   }
+
+  // // Hitta sessionen i sessions-arrayen som matchar token
+  // const session = sessions.find((session) => session.token === token);
+  // console.log("Found session:", session);
+
+  // // Om sessionen hittas, extrahera userId från sessionen
+  // if (session) {
+  //   const userId = session.userId;
+
+  //   // Hitta kontot i accounts-arrayen som matchar userId
+  //   const account = accounts.find((acc) => acc.userId === userId);
+  //   if (account) {
+  //     // Om kontot hittas, skicka tillbaka saldo som JSON-svar
+  //     res.json({ balance: account.balance });
+  //   } else {
+  //     // Om kontot inte hittas, skicka tillbaka status 404 och ett felmeddelande
+  //     res.status(404).json({ error: "Account not found" });
+  //   }
+  // } else {
+  //   // Om sessionen inte hittas, skicka tillbaka status 401 och ett felmeddelande
+  //   res.status(401).json({ error: "Invalid session token" });
+  // }
 });
 
-app.post("/me/account/transaction", (req, res) => {
+app.post("/me/account/transaction", async (req, res) => {
   // Extrahera token från Authorization-headern
   const token = req.headers.authorization.split(" ")[1]; // Extract token from Authorization header
   console.log("Received token:", token);
+   // Extrahera beloppet från förfrågans body
+   const { amount } = req.body;
 
-  // Hitta sessionen i sessions-arrayen som matchar token
-  const session = sessions.find((session) => session.token === token);
+  try {
+    // SQL-förfrågan - hitta sessionen i sessions-tabellen som matchar token
+    const sessionResult = await query("SELECT * FROM sessions WHERE token = ?", [
+      token,
+    ]);
 
-  // Om sessionen hittas
-  if (session) {
-    // Hitta kontot i accounts-arrayen som matchar userId från sessionen
-    const account = accounts.find(
-      (account) => account.userId === session.userId
-    );
-    // Om kontot hittas
-    if (account) {
-      // Extrahera beloppet från förfrågans body
-      const { amount } = req.body;
+    // sessionResult returnerar en array, position 0 är första raden
+    const session = sessionResult[0];
 
-      // Uppdatera kontots saldo med beloppet
-      account.balance += amount;
-
-      // Skicka tillbaka det uppdaterade kontot som JSON-svar
-      res.json(account);
-
-      // Skicka tillbaka en 201-status och ett meddelande med det nya saldot
-      res.status(201).json({ message: account.balance });
-    } else {
-      // Om kontot inte hittas, skicka tillbaka en 404-status och ett felmeddelande
-      res.status(404).json({ message: "Account not found" });
+    // Om det inte finns en session som matchar engångslösenordet
+    if(!session) {
+      return res.status(401).send("Invalid session token");
     }
-  } else {
-    // Om sessionen inte hittas, skicka tillbaka en 401-status och ett felmeddelande
-    res.status(401).json({ message: "Invalid Token" });
-  }
+
+    // Hämta användar-id för sessionen 
+    const userId = session.user_id;
+
+    // SQL-förfrågan - hitta kontot i accounts-tabellen som matchar userId
+    const accountResult = await query(
+      "SELECT * FROM accounts WHERE user_id = ?",
+      [userId]
+    );
+
+    // accountResult returnerar en array, position 0 är första raden
+    const account = accountResult[0];
+
+    // Om inget konto hittades
+    if(!account) {        
+      return res.status(401).send("Account not found");
+    }
+    
+    // Plussa på kontots nuvarande saldo med amount som hämtats från req.body
+    const balance = account.amount + amount;
+
+    // Skicka svar till servern med uppdaterade saldot
+    res.status(200).json({ balance });
+
+} catch (error) {
+  // Fel i databasuppkoppling, serverkod eller annat
+  console.error("Error fetching balance:", error);
+  res.status(500).send("Error fetching balance");
+
+}
+
+  // // Hitta sessionen i sessions-arrayen som matchar token
+  // const session = sessions.find((session) => session.token === token);
+
+  // // Om sessionen hittas
+  // if (session) {
+  //   // Hitta kontot i accounts-arrayen som matchar userId från sessionen
+  //   const account = accounts.find(
+  //     (account) => account.userId === session.userId
+  //   );
+  //   // Om kontot hittas
+  //   if (account) {
+  //     // Extrahera beloppet från förfrågans body
+  //     const { amount } = req.body;
+
+  //     // Uppdatera kontots saldo med beloppet
+  //     account.balance += amount;
+
+  //     // Skicka tillbaka det uppdaterade kontot som JSON-svar
+  //     res.json(account);
+
+  //     // Skicka tillbaka en 201-status och ett meddelande med det nya saldot
+  //     res.status(201).json({ message: account.balance });
+  //   } else {
+  //     // Om kontot inte hittas, skicka tillbaka en 404-status och ett felmeddelande
+  //     res.status(404).json({ message: "Account not found" });
+  //   }
+  // } else {
+  //   // Om sessionen inte hittas, skicka tillbaka en 401-status och ett felmeddelande
+  //   res.status(401).json({ message: "Invalid Token" });
+  // }
 });
 
 // Startar servern
